3e5a842c58ecf399ccd3f65e8b67735b2e10b77f
sujeet123-dot <sujeetkumarak40@gmail.com>
2026-02-27 00:00:42 +0530

    target url change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cookieParser());
 
-const TARGET_URL = "https://www.zenithummedia.com/case-studies?utm_source=google&utm_medium=medium&utm_campaign=NEW307&utm_id=Visit_frame";
+const TARGET_URL = "https://www.zenithummedia.com/case-studies?utm_source=google&utm_medium=medium&utm_campaign=LEMDA&utm_id=Visit_frame";
 const MEASUREMENT_ID = "G-SNCY0K36MC";
 
 /**
